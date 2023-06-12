f6158b50572ca9e129844448e94cacb08d7f56de
Mert <mertsevin58@gmail.com>
2023-06-12 21:29:41 +0300

    Added Collect Coin and Score UI
    Added Collect Coin and Score UI

---
+++ Assets/Scripts/checkCollisions.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class checkCollisions : MonoBehaviour
+{
+    public int score;
+    public TextMeshProUGUI CoinText;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.CompareTag("Coin"))
+        {
+            //Debug.Log("Coin!");
+            AddCoin();
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
+    public void AddCoin()
+    {
+        score++;
+        CoinText.text = "Score: " + score.ToString();
+         
+    }
+
+
+
+}
